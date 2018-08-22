e6f336352da556cbaa8d17c7bb78d9fc232fa4c3
WXZ18 <42071106+WXZ18@users.noreply.github.com>
2018-08-22 16:04:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+    <script type="text/javascript">
+    $(document).ready(function() {
+        $("[src='bmbr.jpg']").css("borderStyle","solid")
+        $("[src='bmbr2.jpg']").css("borderStyle","solid")
+        $("[src='lsliv.jpg']").css("borderStyle","solid");
+        $("[src='backpack.jpg']").css("borderStyle","solid");
+        $("[src='backpack2.jpg']").css("borderStyle","solid");
+        $("[src='backpack3.jpg']").css("borderStyle","solid");
+        $("[src='ph.jpg']").css("borderStyle","solid");
+        $("[src='ph2.jpg']").css("borderStyle","solid");
+        $("[src='ph3.jpg']").css("borderStyle","solid"); 
+
+    </script>
